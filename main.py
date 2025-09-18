4185c676b2e4bf4212a1f381ea2bc10fcacb2a05
Anuar Turlubay <turlybay.anuar909@gmail.com>
2025-09-18 17:12:23 +0500

    templates

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi import Request
+from fastapi.templating import Jinja2Templates
 import uvicorn
 import os
 
 app = FastAPI()
 
-# CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Mount static files
+# Serve static files (CSS, images, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Templates folder
+# Setup templates
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/app", response_class=HTMLResponse)
-async def get_app(request: Request):
+# Routes
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/product", response_class=HTMLResponse)
+async def product(request: Request):
+    return templates.TemplateResponse("product.html", {"request": request})
+
+@app.get("/trainings", response_class=HTMLResponse)
+async def trainings(request: Request):
+    return templates.TemplateResponse("trainings.html", {"request": request})
+
+@app.get("/support", response_class=HTMLResponse)
+async def support(request: Request):
+    return templates.TemplateResponse("support.html", {"request": request})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
