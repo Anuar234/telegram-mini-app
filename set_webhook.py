2026f821e3007f42bcceacb1754279001a24b5af
Anuar Turlubay <turlybay.anuar909@gmail.com>
2025-09-19 23:19:17 +0500

    added webhook but just env

--- set_webhook.py
+++ set_webhook.py
@@ -1,6 +1,8 @@
 import requests
+import os
 
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = os.getenv("BOT_TOKEN") # Добавь BOT_TOKEN в Railway secrets
+
 WEBHOOK_URL = "https://web-production-81447.up.railway.app/webhook"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
