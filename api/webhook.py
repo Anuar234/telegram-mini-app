6697504022f154e5be47fd0d34cb0d7d09aaaebb
Anuar Turlubay <turlybay.anuar909@gmail.com>
2026-02-03 04:58:54 +0500

    added some json

--- api/webhook.py
+++ api/webhook.py
@@ -114,7 +114,7 @@ class handler(BaseHTTPRequestHandler):
             reply_markup = InlineKeyboardMarkup(keyboard)
             
             # Отправляем текстовое сообщение
-            gif_url = "https://telegram-mini-app-silk-five.vercel.app/welcome.gif"
+            gif_url = "https://raw.githubusercontent.com/Anuar234/telegram-mini-app/main/public/welcome.gif"
             try:
                 await bot.send_animation(
                     chat_id=chat_id,
